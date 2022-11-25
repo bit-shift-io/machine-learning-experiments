16ccb9528d96e4e046f73926ddfb73db07057adf
Fabian Mathews <fabian@jpmedia.com.au>
2022-11-25 12:52:13 +1030

    updated to use smaller dataset for testing

--- netlifx-categorizer/train.py
+++ netlifx-categorizer/train.py
@@ -1,149 +1,96 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import os
-import warnings
-import tensorflow as tf
 from tensorflow import keras
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn import preprocessing
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation
-from tensorflow.keras.optimizers import SGD
-
-from keras.utils import to_categorical
-
-# read datasets
-titles_df = pd.read_csv("data/netflix_titles.csv") 
-
-# Only grab first n rows for a smaller dataset
+# read datasets, then only grab first n rows for a smaller dataset
+#titles_df = pd.read_csv("data/netflix_titles.csv") 
+titles_df = pd.read_csv("test-data/test-data-1.csv")
 titles_df = titles_df.iloc[:100]
-
 print(titles_df.head())
 
-# prepare the data
-ids = []
+# https://www.ritchieng.com/machinelearning-one-hot-encoding/
+# convert id's to labels, which then get converted to onehot encoding
+le = preprocessing.LabelEncoder()
+show_id_l = le.fit_transform(titles_df.show_id) #['s1', 's2' ,'s3']) #titles_df.show_id)
+print(show_id_l)
+ohe = preprocessing.OneHotEncoder()
+show_id_oh = ohe.fit_transform(show_id_l.reshape(-1, 1)).toarray()
+print(show_id_oh)
+
+# convert the column 'listed_in' which is a multi-label column (i.e. tags) to use
+# one hot encoding
 listed_in = []
-out_layers = []
-
-onehot_show_ids = pd.get_dummies(titles_df.show_id)
-#print(onehot_show_ids)
-#onehot_show_ids = MultiLabelBinarizer().fit_transform(shows_ids.values)
-
-
 for index, row in titles_df.iterrows():
-    id = int(row.show_id[1:])
-    ids.append(id)
-    listed_in.append(set(row.listed_in.split(', ')))
+    listed_in.append(list(set(row.listed_in.split(', '))))
 
-encoded_ids = to_categorical(ids) #titles_df.shape[0]) # one hot encode ids
-
-mlb = MultiLabelBinarizer() # one hot encoding for multiple labels
+mlb = preprocessing.MultiLabelBinarizer() # one hot encoding for multiple labels
 encoded_listed_in = mlb.fit_transform(listed_in)
 listed_in_width = encoded_listed_in.shape[1]
 print(mlb.classes_)
 
-#d = {'show_id': ids, 'listed_in': encoded_listed_in}
-#prepared_df = pd.DataFrame(data=d)
-
-#dataset = tf.data.Dataset.from_tensor_slices(
-#        (ids, encoded_listed_in)
-#    )
-#dataset = encoded_listed_in
-
-#train_x, test_x = train_test_split(dataset, test_size=0.2, random_state=1)
-
-# _x is the inputs, _y is the outputs
-#train_x, test_x, train_y, test_y = train_test_split(encoded_listed_in, encoded_ids, test_size=0.2, random_state=1)
-
-# https://stackoverflow.com/questions/57349349/using-different-sample-weights-for-each-output-in-a-multi-output-keras-model
-
-inp = keras.layers.Input(shape=(listed_in_width,))
-
-out_layers = []
-
-x = encoded_listed_in # inputs
-y_list = [] # expected outputs
-sample_weight = {} # dictionary of weights for each output layer
-
-
+# compile training data
+x = encoded_listed_in # inputs training dataset
+y_list = show_id_oh # expected outputs for training
+sample_weight = {} # dictionary of weights for each output layer for training set
 
 for index, row in titles_df.iterrows():
-    onehot_id = onehot_show_ids[row.show_id]
-    sample_weight[row.show_id] = onehot_id
-    y_list.append(onehot_id)
+    row_show_id_le = le.transform([row.show_id])
+    row_show_id_ohe = ohe.transform(row_show_id_le.reshape(-1, 1)).toarray()
+    print(row_show_id_le)
+    sample_weight[row.show_id] = row_show_id_ohe.reshape(-1, 1)
     
+
+# construct the model
+inp = keras.layers.Input(shape=(listed_in_width,))
+out_layers = []
+
 for index, row in titles_df.iterrows():
     out = keras.layers.Dense(1, name=row.show_id)(inp)
     out_layers.append(out)
 
 model = keras.models.Model(inp, out_layers)
-model.compile(loss='mse',
-              optimizer='adam')
+model.compile(loss='mse', optimizer='adam')
+print(model.summary())
 
+# learn the data
+history = model.fit(x, y_list, epochs=500, verbose=1, sample_weight=sample_weight)
 
-# TODO: add validation_data - training data!
-history = model.fit(x, y_list, epochs=5, batch_size=32, verbose=1, sample_weight=sample_weight)
+# run a prediction - what shows have the following categories?
+categories = [
+    ['Documentaries'],
+    ['Drama'],
+    ['Documentaries', 'Drama']
+ ] #['Documentaries'] #['Drama'] #['Documentaries', 'Drama']
+categories_enc = mlb.transform(categories)
+print(categories_enc)
+print(titles_df.head())
+for cat_idx, cat_enc in enumerate(categories_enc):
+    reshaped = cat_enc.reshape(-1, cat_enc.shape[0])
+    pred = model.predict(reshaped)
 
+    pred = np.array(pred).reshape(-1) #reshape to single dimension
+    #pred_ids = (-pred).argsort() #[0:5]
+    #print(pred_ids)
 
-#save the model
+    for pred_idx, pred_chance in enumerate(pred):
+        show_id = le.inverse_transform([pred_idx])[0]
+        print(f"{show_id} has {pred_chance} chance of having categories: {categories[cat_idx]}")
+
+    #print(pred)
+
+# save the model
 model.save('model')
 
-'''
-inp = keras.layers.Input(shape=(5,))
-# assign names to output layers for more clarity
-out1 = keras.layers.Dense(1, name='out1')(inp)
-out2 = keras.layers.Dense(1, name='out2')(inp)
-
-model = keras.models.Model(inp, [out1, out2])
-model.compile(loss='mse',
-              optimizer='adam')
-
-
-
-# create some dummy training data as well as sample weight
-n_samples = 100
-X = np.random.rand(n_samples, 5)
-y1 = np.random.rand(n_samples,1)
-y2 = np.random.rand(n_samples,1)
-
-w1 = np.random.rand(n_samples,)
-w2 = np.random.rand(n_samples,)
-
-model.fit(X, [y1, y2], epochs=5, batch_size=16, sample_weight={'out1': w1, 'out2': w2})
-
-
-
-
-# assign names to output layers for more clarity
-#out1 = layers.Dense(1, name='out1')(inp)
-#out2 = layers.Dense(1, name='out2')(inp)
-
-model = Sequential()
-model.add(Dense(100, activation='relu', input_dim=listed_in_width)) #train_x.shape[1]))
-model.add(Dropout(0.1))
-model.add(Dense(50, activation='relu'))
-model.add(Dropout(0.1))
-model.add(Dense(encoded_ids.shape[1], activation='sigmoid'))
-
-model.compile(loss='binary_crossentropy', optimizer="adam", metrics=["categorical_accuracy"])
-
-history = model.fit(encoded_listed_in, encoded_ids, sample_weight=encoded_ids, epochs=5, batch_size=2000)
-
-model.save('model')
-
+# report
 train_loss = history.history['loss']
-#val_loss = history.history['val_loss']
-cat_acc = history.history['categorical_accuracy']
 plt.plot(train_loss, color='r', label='Train Loss')
-#plt.plot(val_loss, color='b', label='Validation Loss')
-plt.plot(cat_acc, color='g', label='Categorical Accuracy')
 plt.xlabel("Epochs")
 plt.title("Train and Validation Loss Curve")
 plt.legend()
 plt.grid()
 plt.show()
 
-'''
+
 print('done')
