b9d78930aa5fa9052bab785844979510d288c4e4
Fabian Mathews <fabian@jpmedia.com.au>
2023-01-27 17:05:49 +1030

    filtering out more bad data

--- web-gen/crawler.js
+++ web-gen/crawler.js
@@ -17,7 +17,7 @@ const __dirname = path.dirname(__filename)
 
 
 const TEST_SINGLE = false
-
+const TEST_SCREENSHOT_DISABLED = false
 
 async function getPropertyValue(element, property) {
     return await (await element.getProperty(property)).jsonValue()
@@ -61,17 +61,12 @@ function computeBounds(boundingBox, styles) {
 
 // TODO: if a container only has a single child with no margins, ignore it as it can be flattened with the child....
 async function handleElement(page, parent_element, parent_bounds, element, dir) { 
-    const MIN_SIZE = 10
+    const MIN_SIZE = 20
     const MIN_ENTROPY = 0.2
 
     //const cls = await getPropertyValue(element, 'class') 
     const id = await getPropertyValue(element, 'id')
     const tag_name = (await getPropertyValue(element, 'tagName')).toLowerCase()
-
-    /*
-    if (dir == 'data/cloudflare-com/body/child_0/child_0/child_0/child_2/child_4/child_0/child_0/child_0/child_1/child_0/child_2/child_0') {
-        debugger
-    }*/
 
     // we dont care about iframes or other tags
     const VALID_TAGS = ['div', 'body', 'a', 'ul', 'li']
@@ -84,8 +79,13 @@ async function handleElement(page, parent_element, parent_bounds, element, dir) 
     })
 
     // TODO: add other css properties we are interested in
-    const pickedStyles = pick(styles, ['display', 'flexDirection', 'flex', 'marginTop', 'marginLeft', 'marginRight', 'marginBottom', 'paddingTop', 'paddingLeft', 'paddingRight', 'paddingBottom'])
-    if (pickedStyles.display == 'none') {
+    const pickedStyles = pick(styles, ['display', 'float', 'position', 'visibility', 'flexDirection', 'flex', 'marginTop', 'marginLeft', 'marginRight', 'marginBottom', 'paddingTop', 'paddingLeft', 'paddingRight', 'paddingBottom'])
+    if (pickedStyles.display == 'none' || pickedStyles.visibility == 'hidden') {
+        return null
+    }
+
+    // element could be anywhere!
+    if (pickedStyles.position == 'absolute') {
         return null
     }
 
@@ -108,19 +108,6 @@ async function handleElement(page, parent_element, parent_bounds, element, dir) 
         boundsRelativeToParent = makeRelativeToParent(boundingBox, parent_bounds)
     }
 
-    // boil the layout down to row or column
-    // TODO: handle grid
-    let layout = 'column'
-    if (pickedStyles.display == 'flex') {
-        layout = pickedStyles['flexDirection'] || 'row'
-    }
-
-    // we could handle this by getting the parent to check if its children contain 'inline-block'
-    // then nchange its layout to a 'row'.... for now if we see inline block abort!
-    if (pickedStyles.display == 'inline-block') {
-        return null
-    }
-
     const r_children = []
     const children = await element.$$(':scope > *')
     for (let i = 0; i < children?.length; ++i) {
@@ -131,16 +118,61 @@ async function handleElement(page, parent_element, parent_bounds, element, dir) 
         }
     }
 
+    /*
+    if (dir == 'data/namebright-com/body/child_1/child_0/child_0/child_0/child_1/child_0/child_0/child_7/child_1') {
+        debugger
+    }*/
+
+    // boil the layout down to row or column
+    // TODO: handle grid
+    let layout = 'column'
+    if (pickedStyles.display == 'flex') {
+        layout = pickedStyles['flexDirection'] || 'row'
+    }
+    // we could handle this by getting the parent to check if its children contain 'inline-block'
+    // then change its layout to a 'row' or 'column' as appropriate.... 
+    else if (pickedStyles.display == 'inline' || pickedStyles.display == 'inline-block' || pickedStyles.display == 'block') {
+        if (r_children.length) {
+            const firstChildDisplay = r_children[0].css.display
+            //const childrenDisplay = r_children.map(c => c.css.display)
+            if (firstChildDisplay == 'inline' || firstChildDisplay == 'inline-block') {
+                layout = 'row'
+            } else {
+                layout = 'column'
+            }
+
+            const floatChild = r_children.find(c => c.css.float == 'left' || c.css.float == 'right')
+            if (floatChild) {
+                return null // it could be any weird layout!
+            }
+        }
+    } else {
+        //console.log(`unhandled display: ${pickedStyles.display} for: ${dir}`)
+        return null
+    }
+
+    const client_rect = (await element.evaluate((element) => {
+        return element.getClientRects()
+    }))[0]
+
     // now we really only care about the height of the first child in the case of a column layout
     // and in the case of a row layout we only care about the width
     let firstChildSize = {width: boundsRelativeToParent.width, height: boundsRelativeToParent.height}
     let firstChild = r_children?.[0]
     if (firstChild) {
         if (layout == 'column') {
-            firstChildSize.height = firstChild.bounds.height
+            //firstChildSize.height = firstChild.bounds.height //+ parseFloat(pickedStyles.paddingTop)
+            firstChildSize.height = firstChild.client_rect.bottom - client_rect.top
+            //console.log('qwe')
         } else {
-            firstChildSize.width = firstChild.bounds.width
+            //firstChildSize.width = firstChild.bounds.width //+ parseFloat(pickedStyles.paddingLeft)
+            firstChildSize.width = firstChild.client_rect.right - client_rect.left
         }
+    }
+
+    // why is the child so small?
+    if (firstChildSize.width < MIN_SIZE || firstChildSize.height < MIN_SIZE) {
+        return null
     }
 
     const img_path = `${dir}/screenshot.jpg`
@@ -153,6 +185,7 @@ async function handleElement(page, parent_element, parent_bounds, element, dir) 
         layout,
         first_child_size: firstChildSize,
 
+        client_rect,
         parent_size: { // need parent size to compute fractional scaling
             width: parent_bounds?.width || boundingBox.width,
             height: parent_bounds?.height || boundingBox.height,
@@ -178,19 +211,21 @@ async function handleElement(page, parent_element, parent_bounds, element, dir) 
             y: boundingBox.y + await page.evaluate(() => window.scrollY),
         }
 
-        // Take an area screenshot.
-        const areaScreenshot = await page.screenshot({ path: img_path, clip: clipRelativeToPage, fullPage: true })
+        if (!TEST_SCREENSHOT_DISABLED) {
+            // Take an area screenshot.
+            const areaScreenshot = await page.screenshot({ path: img_path, clip: clipRelativeToPage, fullPage: true })
 
-        let sImg = sharp(img_path)
-        sImg = await sImg.resize(200, 200, { fit: 'fill' })
+            let sImg = sharp(img_path)
+            sImg = await sImg.resize(200, 200, { fit: 'fill' })
 
-        const stats = await sImg.stats()
-        if (stats.entropy < MIN_ENTROPY) {
-            //throw `low entropy for: ${img_path}`
-            return null
+            const stats = await sImg.stats()
+            if (stats.entropy < MIN_ENTROPY) {
+                //throw `low entropy for: ${img_path}`
+                return null
+            }
+            
+            await sImg.toFile(img_path_200)
         }
-        
-        await sImg.toFile(img_path_200)
 
     } catch (err) {
         try {
@@ -273,8 +308,7 @@ if (TEST_SINGLE) {
     const browser = await chromium.launch()
     //const url = 'https://www.wikipedia.org/'
     let url = `file://${__dirname}/test-web/test-1.html`
-    url = `https://www.cambridge.org/`
-    url = `https://www.cloudflare.com/`
+    url = `https://www.namebright.com/`
     await screenshotWebsite(browser, url)
     await browser.close()
 } else {
