f0c9edf66efb4caa2a7e743046536f200afff7c4
Fabian Mathews <supagu@gmail.com>
2023-02-08 10:54:01 +1030

    removed web-gen into its own repo

--- web-gen/install.bat
+++
@@ -1,3 +0,0 @@
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu117
-
-pip3 install matplotlib scikit-learn tqdm torchshape seaborn
--- web-gen/install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python3 -m pip install --upgrade pip
-
-pip3 install torch pillow torchshape
